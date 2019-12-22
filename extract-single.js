08ab0f25c331d2d40154bd685b1293a63b42d489
bguiz <bguiz@users.noreply.github.com>
2019-12-22 12:50:08 +0800

    feat: add extractSingle that parses PDF via pdf2json, then manually processes the data

---
+++ extract-single.js
@@ -0,0 +1,92 @@
+const path = require('path');
+const pdf2Json = require('pdf2json');
+const dateParse = require('date-fns/parse');
+const dateFormatIso = require('date-fns/formatISO');
+
+const { trimAndCamelCase } = require('./util.js');
+
+function processPdfText (text) {
+  const lines = text.split('\n');
+  const object = lines.map((unparsedLine, index) => {
+    const line = unparsedLine.replace('\r', '').trim();
+    if (line === '' ||
+        index === 0 ||
+        index >= lines.length - 4) {
+      return {};
+    } else if (index === 1) {
+      return {
+        licencePlate: line.trim(),
+      };
+    } else if (line === 'CASH') {
+      // What are the other payment modes?
+      return {
+        paymentMode: line,
+      };
+    } else if (line.indexOf('$') > 0) {
+      const parts = line.split('$');
+      const key = trimAndCamelCase(parts[0]);
+      const value = parseFloat(parts[1].trim());
+      return {
+        [key]: value,
+      };
+    } else {
+      const parts = line.split(/[\s]{2,}/);
+      const key = trimAndCamelCase(parts[0]);
+      let value = parts[1].trim();
+      if (key === 'distanceRun') {
+        value = parseFloat(value.replace(' KM', ''));
+      } else if (key === 'start' || key === 'end') {
+        const parsedDate = dateParse(value, 'dd/MM/yyyy HH:mm', new Date());
+        value = dateFormatIso(parsedDate);
+      }
+      return {
+        [key]: value,
+      };
+    }
+  }).reduce(
+    (x, acc) => ({ ...x, ...acc }),
+    {},
+  );
+  return object;
+}
+
+function extractSingle(pdfPath) {
+  return new Promise((accept, reject) => {
+    let pdfParser = new pdf2Json(this, 1);
+
+    pdfParser.on(
+      'pdfParser_dataError',
+      (errData) => {
+        console.error(errData.parserError);
+        return reject(errData.parserError);
+      }
+    );
+
+    pdfParser.on('pdfParser_dataReady', (pdfData) => {
+      const text = pdfParser.getRawTextContent();
+      const object = processPdfText(text);
+      return accept({
+        text,
+        object,
+      });
+    });
+
+    pdfParser.loadPDF(pdfPath);
+  });
+}
+
+module.exports = {
+  extractSingle,
+  processPdfText,
+};
+
+async function demoExtractSingle () {
+  const pdfPath = path.resolve(process.argv[2]);
+  const results = await extractSingle(pdfPath);
+  console.log(results.text);
+  console.log(results.object);
+}
+
+if (process.env.DEMO_EXTRACT_SINGLE) {
+  demoExtractSingle();
+}
